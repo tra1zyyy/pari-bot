85502954dcc92b84eda1b0c1d2269f2f3ddb4b91
Student <artem.rudakovsky@gmail.com>
2024-05-27 14:32:48 +0300

    init

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import asyncio
 
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import F
 from aiogram.filters.state import StateFilter
 from userstates import UserStates
 from keyboardhelper import keyboards
+
+import pariservice as ps
 
 from config import TOKEN
 
@@ -16,13 +19,26 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def send_welcome(message: types.Message, state: FSMContext):
     kb = keyboards[UserStates.BASE]
-    await bot.send_message(message.chat.id, "Привет, я эхо-бот!", reply_markup=kb)
+    await  message.answer("Привет, я эхо-бот!", reply_markup=kb)
     await state.set_state(UserStates.BASE)
 
 @dp.message(Command("test"), StateFilter(UserStates.BASE))
 async def send_welcome(message: types.Message):
     await message.answer('Я в состоянии BASE')
 
+
+@dp.message(F.text == "Мои пари", StateFilter(UserStates.BASE))
+async def moi_pari(message: types.Message):
+    text = "Ваши пари"
+    paris = ps.get_paris(message.from_user.id)
+    for pari in paris:
+        text += "\n" + pari
+    await message.answer(text)
+
+@dp.message(F.text == "Создать пари", StateFilter(UserStates.BASE))
+async def create_pari(message: types.Message):
+    text = ps.add_pari(message.from_user.id, message.text)
+    await message.answer(text)
 
 async def main():
     await dp.start_polling(bot)
---
+++ pariservice.py
@@ -0,0 +1,5 @@
+def add_pari(user_id, text):
+    message = "add pari \"" + text  +"\"and your id" + str(user_id)
+    return message
+def get_paris(user_id):
+    return["Test 1, Test 2, Test 3"]
